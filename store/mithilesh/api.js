5022dbf21efbd3741998f3ed4b2daa805b0ef6c6
Crowdbotics <engineering@crowdbotics.com>
2023-07-20 10:33:53 +0000

    Update store.

--- store/mithilesh/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios"
-import { MITHILESH_USERNAME, MITHILESH_PASSWORD } from "react-native-dotenv"
-const mithilesh = axios.create({
-  baseURL: "https://connector.com",
-  auth: { username: MITHILESH_USERNAME, password: MITHILESH_PASSWORD },
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
-export const apiService = {}
